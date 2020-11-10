f76e52e933dbfa3bf49e098e3679d76d93dec382
MohamedAitouna <aitounam@gmail.com>
2020-11-10 23:04:36 +0100

    Phaser-Start: Start Sanke Game - set borders and Move the snake Head :D

--- src/app/core/hello-home/phaserStates/menu-state.ts
+++ src/app/core/hello-home/phaserStates/menu-state.ts
@@ -10,6 +10,7 @@ export class MenuState extends Phaser.Scene {
     // const loadSpinner = this.add.sprite(0, 0, 'loadSpinner', loadFrame);
     // loadSpinner.setOrigin(0, 0);
     this.load.on('progress', value => {
+      console.log('progress');
       // loadFrame = (loadFrame + 1) % 3;
       // loadSpinner.setFrame(loadFrame);
     });
@@ -18,12 +19,19 @@ export class MenuState extends Phaser.Scene {
     });
     this.load.on('complete', () => {
       // loadSpinner.destroy();
+      console.log('complete');
     });
 
     this.load.image('Mister0', '../../../../assets/img/HelloPage/Mister0.png');
+    this.load.image('snakeHead', '../../../../assets/img/HelloPage/snake/snakeHead.bmp');
+    this.load.image('snakeBody', '../../../../assets/img/HelloPage/snake/snakeBody.bmp');
+    this.load.image('snakeFood', '../../../../assets/img/HelloPage/snake/snakeFood.bmp');
+    this.load.image('border', '../../../../assets/img/HelloPage/snake/border.bmp');
+
 
   }
   create() {
+    console.log('create');
     const mister0 = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Mister0')
       .setInteractive();
     mister0.setOrigin(0.5, 0.5);
--- src/app/core/hello-home/phaserStates/snake-state.ts
+++ src/app/core/hello-home/phaserStates/snake-state.ts
@@ -1,11 +1,53 @@
 import Phaser from 'phaser';
 
 export class SnakeState extends Phaser.Scene {
+  snakeHead: Phaser.GameObjects.Image;
+  borderDown: Phaser.GameObjects.Image;
+  borderUp: Phaser.GameObjects.Image;
+  start = false;
+  directionY = 1;
   constructor() {
     super({ key: 'snake' });
   }
   preload() {
   }
   create() {
+
+    // Set Border
+    this.borderUp = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 4, 'border');
+    this.borderUp.setOrigin(0.5, 0);
+    this.borderDown = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'border');
+    this.borderDown.setOrigin(0.5, 0);
+    const positionX = this.borderUp.x;
+    const positionY = this.borderUp.y;
+    const height = this.borderUp.height;
+    this.snakeHead = this.add.image(positionX, positionY + height, 'snakeHead').setOrigin(0.5, 0);
+
+    const keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    keyDown.on('down', () => {
+      this.start = true;
+    });
+
+  }
+  update(time: number, delta: number) {
+    if (this.start) {
+      if (this.directionY === 1) {
+        const nextYPosition = this.snakeHead.y + 1;
+        if (nextYPosition < this.borderDown.y - this.borderDown.height) {
+          this.snakeHead.setY(nextYPosition);
+        } else {
+          this.directionY = -1;
+        }
+      } else if (this.directionY === -1) {
+        const nextYPosition = this.snakeHead.y - 1;
+        if (nextYPosition > this.borderUp.y + this.borderDown.height) {
+          this.snakeHead.setY(nextYPosition);
+        } else {
+          this.directionY = 1;
+        }
+      }
+
+    }
+
   }
 }
